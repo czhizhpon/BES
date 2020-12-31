ae02d3790a158665802b7d2066fb5b5f1d61ee34
czhizhpon <czhizhpon@est.ups.edu.ec>
2020-12-31 00:19:01 -0500

    TES-1 #comment Trabajando #in-progress

--- src/ec/edu/ups/view/Main.java
+++ src/ec/edu/ups/view/Main.java
@@ -3,7 +3,7 @@ package ec.edu.ups.view;
 public class Main {
 
     public static void main(String[] args) {
-	FormGUI f = new FormGUI();
-	f.setVisible(true);
+		FormGUI f = new FormGUI();
+		f.setVisible(true);
     }
 }
